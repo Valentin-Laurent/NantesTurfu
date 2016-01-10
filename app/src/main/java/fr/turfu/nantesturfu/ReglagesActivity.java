c3e4b8af73996a5da9eec0a9ea544541195a64da
Valentin Laurent <valentin_laurent@msn.com>
2016-01-10 18:54:38 +0100

    Amélioration de la recherche. Il reste plus qu'à remplir l'activité "ResultatRechercheActivity".

--- app/src/main/java/fr/turfu/nantesturfu/ReglagesActivity.java
+++ app/src/main/java/fr/turfu/nantesturfu/ReglagesActivity.java
@@ -2,15 +2,10 @@ package fr.turfu.nantesturfu;
 
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
-import android.support.v7.app.ActionBar;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuInflater;
 import android.view.View;
 import android.widget.LinearLayout;
-
-import java.util.List;
 
 public class ReglagesActivity extends PreferenceActivity {
 
@@ -22,13 +17,13 @@ public class ReglagesActivity extends PreferenceActivity {
         setContentView(R.layout.activity_reglages);
 
     }
-    //Code recopié (la classe PreferenceActivity gère très mal les toolbars, c'est une solution "bricolage")
+    //Code recopié (la classe PreferenceActivity gère très mal les toolbars, c'est une solution "bricolage", qui utilise une autre toolbar : "toolbar_simplifiee")
     @Override
     protected void onPostCreate(Bundle savedInstanceState) {
         super.onPostCreate(savedInstanceState);
 
         LinearLayout root = (LinearLayout)findViewById(android.R.id.list).getParent().getParent().getParent();
-        Toolbar bar = (Toolbar) LayoutInflater.from(this).inflate(R.layout.reglages_toolbar, root, false);
+        Toolbar bar = (Toolbar) LayoutInflater.from(this).inflate(R.layout.toolbar_simplifiee, root, false);
         root.addView(bar, 0);
         bar.setNavigationOnClickListener(new View.OnClickListener() {
             @Override
@@ -36,6 +31,9 @@ public class ReglagesActivity extends PreferenceActivity {
                 finish();
             }
         });
+
+        //On set le titre qui sera affiché dans la toolbar
+        bar.setTitle("Réglages");
     }
 
     //Permet de ne pas avoir d'animation à la fermeture de l'activité
--- app/src/main/java/fr/turfu/nantesturfu/ResultatRechercheActivity.java
+++ app/src/main/java/fr/turfu/nantesturfu/ResultatRechercheActivity.java
@@ -1,20 +1,45 @@
 package fr.turfu.nantesturfu;
 
-import android.app.Activity;
 import android.app.ListActivity;
+import android.app.SearchManager;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.LinearLayout;
 
-public class ResultatRechercheActivity extends Activity {
+public class ResultatRechercheActivity extends ListActivity {
+
+    String recherche; //La chaine de caractère qui contiendra ce que l'utilisateur à recherché.
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_resultat_recherche);
 
+        //On récupère l'intent généré par la recherche
         Intent i = getIntent();
         if (Intent.ACTION_SEARCH.equals(i.getAction())) {
+            recherche = i.getStringExtra(SearchManager.QUERY);
         }
+    }
+
+    //Code recopié (la classe ListActivity gère très mal les toolbars, c'est une solution "bricolage", qui utilise une autre toolbar : "toolbar_simplifiee")
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+
+        LinearLayout root = (LinearLayout)findViewById(android.R.id.list).getParent().getParent().getParent();
+        Toolbar bar = (Toolbar) LayoutInflater.from(this).inflate(R.layout.toolbar_simplifiee, root, false);
+        root.addView(bar, 0);
+        bar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+        //On set le titre qui sera affiché dans la toolbar
+        bar.setTitle("Recherche pour : "+recherche);
     }
 }
