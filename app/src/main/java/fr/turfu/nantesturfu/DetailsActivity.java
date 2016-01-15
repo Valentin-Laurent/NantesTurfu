7ef51ebd7a85ea82affa711325b58145c340395a
Valentin Laurent <valentin_laurent@msn.com>
2016-01-15 10:39:00 +0100

    Création de l'activité Détails

---
+++ app/src/main/java/fr/turfu/nantesturfu/DetailsActivity.java
@@ -0,0 +1,23 @@
+package fr.turfu.nantesturfu;
+
+import android.app.ListActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DetailsActivity extends ListActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_details);
+        overridePendingTransition(0, 0); //Permet de ne pas avoir d'animation à l'ouverture de l'activité
+    }
+
+
+    //Permet de ne pas avoir d'animation à la fermeture de l'activité
+    @Override
+    public void onPause() {
+        super.onPause();
+        overridePendingTransition(0, 0);
+    }
+}
