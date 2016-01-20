fcfd6d670e1e17a1fcbea72878f3b2f5df6d18b7
Valentin Laurent <valentin_laurent@msn.com>
2016-01-20 15:11:39 +0100

    La MainActivity (qui gère les favoris) est fonctionelle.

--- app/src/main/java/fr/turfu/nantesturfu/MainActivity.java
+++ app/src/main/java/fr/turfu/nantesturfu/MainActivity.java
@@ -13,6 +13,10 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import java.io.BufferedReader;
@@ -30,7 +34,7 @@ import java.util.ArrayList;
 //Factoriser le code qui gère la toolbar
 //Licenses etc.
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemClickListener { //On a besoin d'implémenter OnItemClickListener pour avoir des favoris cliquables
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,6 +45,7 @@ public class MainActivity extends AppCompatActivity {
         if (p.equals("1")) {
             Intent intent2 = new Intent(this, MapActivity.class);
             startActivity(intent2);
+            this.finish(); //On ferme cette activité sinon en faisant "retour" l'utilisateur reviendra sur cette activité ce qui n'est pas logique
         }
 
         setContentView(R.layout.activity_main);
@@ -52,7 +57,20 @@ public class MainActivity extends AppCompatActivity {
         menu.setDisplayShowTitleEnabled(false); //On n'affiche pas le titre de l'appli
         toolbar.setTitle("Favoris");//On affiche par contre le titre de l'activité
 
+        //On récupère les favoris dans une liste :
+        GestionFavoris gestionFav = new GestionFavoris(getApplicationContext());
+        ArrayList<String> arrayFavoris = gestionFav.getFav();
+        if (arrayFavoris.size()==0) { //Si l'utilisateur n'a pas de favoris :
+            arrayFavoris.add("Vous n'avez pas de favoris");
+        } else {                    //Sinon :
+            arrayFavoris.add(0,"Cliquez sur un résultat pour afficher le détail");
+        }
 
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,arrayFavoris);
+        ListView listView = (ListView) findViewById(R.id.listeFavoris);
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(this); //Pour lancer la méthode onItemClic
 
     }
 
@@ -93,6 +111,14 @@ public class MainActivity extends AppCompatActivity {
             default:
                 return super.onOptionsItemSelected(item);
 
+        }
+    }
+
+    public void onItemClick(AdapterView<?> l, View v, int position, long id) {
+        if (position > 0) {
+            Intent intent = new Intent(this, DetailsActivity.class);
+            //intent.putExtra("position", position);
+            startActivity(intent);
         }
     }
 
