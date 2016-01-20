39afcf4c44617a4810c87c3973a546bb9f9acb78
Valentin Laurent <valentin_laurent@msn.com>
2016-01-20 09:16:46 +0100

    test pour voir si j'arive à lancer MapActivity à la place de la main en lisant les préférences

--- app/src/main/java/fr/turfu/nantesturfu/MainActivity.java
+++ app/src/main/java/fr/turfu/nantesturfu/MainActivity.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.SearchView;
@@ -34,6 +35,13 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        //On lance l'activité map à la place de celle-ci suivant les préférences de l'utilisateur
+        int p = PreferenceManager.getDefaultSharedPreferences(getApplicationContext()).getInt("Menu par défaut", 0);
+        if (p == 1) {
+            Intent intent2 = new Intent(this, MapActivity.class);
+            startActivity(intent2);
+        }
 
         setContentView(R.layout.activity_main);
 
