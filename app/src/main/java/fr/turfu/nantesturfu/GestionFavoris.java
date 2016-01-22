b70bf2c1f82c14759635e5c54640a05efc7e0a60
Valentin Laurent <valentin_laurent@msn.com>
2016-01-22 15:53:36 +0100

    GestionFavoris : javadoc

--- app/src/main/java/fr/turfu/nantesturfu/GestionFavoris.java
+++ app/src/main/java/fr/turfu/nantesturfu/GestionFavoris.java
@@ -5,15 +5,23 @@ import android.content.Context;
 import java.util.ArrayList;
 
 /**
- * Created by Valentin on 15/01/2016.
+ * Cette classe utilise la classe TinyDB et permet de gérer très facilement les favoris
  */
 public class GestionFavoris {
     private TinyDB tinydb;
 
+    /**
+     *Le constructeur est simple, il suffit de passer le context
+     * @param context
+     */
     public GestionFavoris(Context context) {
         tinydb = new TinyDB(context);
     }
 
+    /**
+     * Ajoute un favoris
+     * @param nom Le nom du favoris
+     */
     public void addFav(String nom) {
         ArrayList<String> listeFav = tinydb.getListString("cléFav");
         listeFav.add(nom);
@@ -21,15 +29,26 @@ public class GestionFavoris {
 
     }
 
+    /**
+     * Récupère les favoris
+     * @return la liste des favoris
+     */
     public ArrayList<String> getFav() {
         return tinydb.getListString("cléFav");
     }
 
+    /**
+     * Supprimer tous les favoris (méthode qui n'est jamais appelée, utile pour débugger
+     */
     public void clearFav() {
         ArrayList<String> listeFav = new ArrayList<>();
         tinydb.putListString("cléFav",listeFav);
     }
 
+    /**
+     * Supprime un favoris
+     * @param nom Le nom du favoris a supprimer
+     */
     public void deleteFav(String nom) {
         ArrayList<String> listeFav = tinydb.getListString("cléFav");
         listeFav.remove(nom);
